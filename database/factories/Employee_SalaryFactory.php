d9f655e8cf743aafd11dd37faf97740f70fb00e1
Tynneth Jhoel Fuentes <pheonix0014@gmail.com>
2023-08-10 04:55:00 +0800

    added Employee_Salary Factory file

---
+++ database/factories/Employee_SalaryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee_Salary>
+ */
+class Employee_SalaryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): 
+    {
+        return [
+            //
+        ];
+    }
+}
