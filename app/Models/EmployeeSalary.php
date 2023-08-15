53ef9b77754e490331a1443369e95eab6cf2c84c
Tynneth Jhoel Fuentes <pheonix0014@gmail.com>
2023-08-15 19:42:47 +0800

    redo EmployeeSalary Model

--- app/Models/EmployeeSalary.php
+++ app/Models/EmployeeSalary.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Employee_Salary extends Model
+class EmployeeSalary extends Model
 {
     use HasFactory;
 }
